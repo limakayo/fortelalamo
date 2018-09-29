84287e6b2953a36a71aa43502c7288e2078b530a
Kayo <lima.kayo@gmail.com>
2018-09-29 14:11:51 -0300

    changed background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Forte Álamo</h1>
-        </header>
-      </div>
+     <div className="castle"></div>
     );
   }
 }
